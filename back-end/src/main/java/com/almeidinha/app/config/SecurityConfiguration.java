c8e18cde8db97dc90443f3e3c86c6243fe562dfe
marcos.almeida <marcos.almeida@neogrid.com>
2022-05-10 01:47:43 -0300

    test bith in memory auth

---
+++ back-end/src/main/java/com/almeidinha/app/config/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.almeidinha.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // httpSecurity.authorizeRequests().anyRequest().permitAll();
+        httpSecurity
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+        httpSecurity.formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("almeida")
+                .password(passwordEncoder().encode("password"))
+                .authorities("USER", "ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
