2aa148235d5169f8e5074df3af2b5c5dceaf968f
marcos.almeida <marcos.almeida@neogrid.com>
2022-05-10 02:21:49 -0300

    Add configuration, user and auth entities

--- back-end/src/main/java/com/almeidinha/app/config/SecurityConfiguration.java
+++ back-end/src/main/java/com/almeidinha/app/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         httpSecurity.formLogin();
+        httpSecurity.httpBasic();
     }
 
     @Override
